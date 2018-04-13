66d4badc7f544848cf5cafc2de13bda4e4619bfe
Radivarig <reslav.hollos@gmail.com>
2018-04-13 17:37:42 +0200

    Add host from process and default port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import app from "./app.js"
 
+app.set ("port", process.env.PORT || 7777) // eslint-disable-line no-undef
+app.set ("host", process.env.HOST) // eslint-disable-line no-undef
+
 app.listen (app.get ("port"), () => {
   // eslint-disable-next-line no-console
   console.log (`Server listening on port ${app.get ("port")}`)
